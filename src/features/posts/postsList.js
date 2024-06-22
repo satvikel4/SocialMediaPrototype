4267dd3d652f79041d0d38da0122ca3e8e77f69a
Satvik Eltepu <satvik09@hotmail.com>
2024-06-21 17:16:45 -0700

    added dates to posts and sorted by newest

--- src/features/posts/postsList.js
+++ src/features/posts/postsList.js
@@ -5,20 +5,22 @@ import { Link } from 'react-router-dom'
 export const PostsList = () => {
   const posts = useSelector(state => state.posts)
 
-  const renderedPosts = posts.map(post => (
+  // Sort posts in reverse chronological order by datetime string
+const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+const renderedPosts = orderedPosts.map(post => {
+  return (
     <article className="post-excerpt" key={post.id}>
       <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <Link to={`/posts/${post.id}`} className="button muted-button">
         View Post
       </Link>
     </article>
-  ))
-
-  return (
-    <section className="posts-list">
-      <h2>Posts</h2>
-      {renderedPosts}
-    </section>
   )
+})
 }
